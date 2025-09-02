a4ca36e2009d5de66c9a70a3f7fe1e7c27e8c9eb
Muthulakshmy <lakshmym147@gmail.com>
2025-09-02 13:20:02 +0530

    feat:vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
-import { worker } from "./api/worker";
+// import { worker } from "./api/worker";
 
 // Start MSW in development
 // if (process.env.NODE_ENV === "development") {
