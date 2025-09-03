cc8a87f1a7cbe7f6d100fd5fe79e64aee84fb782
Muthulakshmy <lakshmym147@gmail.com>
2025-09-03 13:08:18 +0530

    feat:vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,34 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tsconfigPaths from 'vite-tsconfig-paths'
 
 
 // export default defineConfig({
 // plugins: [react(), tsconfigPaths()],
 // })
 
+// export default defineConfig({
+// plugins: [react(), tsconfigPaths()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://image-gallery-19jyff1ws-muthulakshmys-projects.vercel.app",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
+
+import { defineConfig } from "vite";
+const react = require("@vitejs/plugin-react");
+const tsconfigPaths = require("vite-tsconfig-paths");
+
 export default defineConfig({
-plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     proxy: {
       "/api": {
@@ -27,3 +44,4 @@ plugins: [react(), tsconfigPaths()],
     },
   },
 });
+
